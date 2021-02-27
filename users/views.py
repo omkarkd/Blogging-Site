90dcaf7095f670eb5ae411e943aa4fcdd1099454
omkarkd <kadam.omkar05@gmail.com>
2021-02-27 13:17:59 +0530

    1st commit for day 3

--- users/views.py
+++ users/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def Register(request):
     if request.method == 'POST' :   
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        form.save()
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for {username}')
-        return redirect('blog-home')
+            messages.success(request,f'Your account has being created! You are now able to log in')
+        return redirect('login')
 
     else:       
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request,'users/register.html',{'form': form })
+
+@login_required
+def Profile(request):
+    return render(request,'users/Profile.html')
